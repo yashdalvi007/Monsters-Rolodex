846de39e40df942c11d8908fd9ca959aa4aec3b1
Yash Dalvi <yashdalvi107@gmail.com>
2020-06-20 17:08:07 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  render(){
+
+    const {monsters, searchField} = this.state;
+
+    const filteredMonsters = monsters.filter(monster=>{
+      if (monster.name.toLowerCase().includes(searchField.toLowerCase())){
+        return monster.name
+      } })
+
+      const handleChange = e =>{
+        this.setState({searchField: e.target.value})
+      }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters-Rolodex</h1>
+      <input className='search' type='search' placeholder='search monster' onChange = {handleChange} />
+        <CardList monsters={filteredMonsters} />
     </div>
   );
+}
 }
 
 export default App;
